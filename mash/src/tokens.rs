83fa9417d10a28ac824ca7a4d80a8483dd186f78
Jeremy Smith <jeremy.ws.2008@gmail.com>
2025-10-01 22:54:22 -0500

    Did craaaazy amounts of stuff: a. TRIED to fix command execution by implementing a process pool to stop processes from dropping b. Failed, changed stdin and out piping between commands

--- mash/src/tokens.rs
+++ mash/src/tokens.rs
@@ -1,6 +1,8 @@
-use std::{io::Read, process::{self, Command}};
+#![allow(dead_code)]
+
+use std::{collections::VecDeque, io, process::{Child, Command}};
 use std::process::Stdio;
-use std::io;
+
 /*
 Rules are pretty much identical to most traditional shells:
     - Every command ends either with a newline or a redirection
@@ -15,31 +17,104 @@ struct CommandParams{
     args: Vec<String>
 }
 
+#[derive(PartialEq, Eq)]
 enum Terminator{
     Pipe,
-    End,
+    EndCmd,
+    None,
 }
 
 impl CommandParams{
-    fn execute_statement(commands: Vec<(CommandParams, Terminator)>){
-        for (command, term) in commands{
+    fn execute_statement(commands: Vec<(CommandParams, Terminator)>) -> io::Result<()>{
+        let mut io_out: Option<Stdio> = None;
+        let mut all_spawned: VecDeque<Child> = VecDeque::with_capacity(commands.len());
 
+        for i in 0..commands.len(){
+            let command: &CommandParams;
+            let terminator_before: &Terminator;
+            let terminator_after: &Terminator;
+            if i == 0{
+                terminator_before = &Terminator::None;
+            }
+            else { terminator_before = &commands[i - 1].1}
+            terminator_after = &commands[i].1;
+            command = &commands[i].0;
+
+            let mut input_from: Option<Stdio> = None;
+            let mut output_to: Option<Stdio> = None;
+
+            if *terminator_before == Terminator::Pipe {
+                input_from = io_out.take();
+            }
+
+            if *terminator_after == Terminator::Pipe {
+                output_to = Some(Stdio::piped());
+            }
+
+            let mut child = command.exec_self::<Stdio>(input_from, output_to)?;
+
+            if *terminator_after == Terminator::Pipe {
+                io_out = Some(child.stdout.take().expect("Error grabbing a process's stdout").into());
+            } else {
+                io_out = None;
+            }
+            
+            all_spawned.push_back(child);
         }
+        //EXIT SIGNAL HERE
+        'continuous: loop{
+            let mut i = 0;
+            while i < all_spawned.len() {
+                if all_spawned[i].try_wait()?.is_some() {
+                all_spawned.remove(i);
+                } 
+                else {
+                    i += 1;
+                }
+            }
+        }
+        return Ok(());
     }
 
-    fn exec_self<T>(&self, input_from: Option<T>) -> std::io::Result<Stdio>
+    /// THIS COMMAND ASSUMES THE INPUT_FROM PIPE IS STILL ACTIVE ON CALL-TIME
+    fn exec_self<T>(&self, input_from: Option<T>, output_to: Option<T>) -> std::io::Result<Child>
     where T: Into<Stdio>
     {
-        let mut exec = &mut Command::new(self.invoking.clone());
-        exec = exec.args(self.args.clone());
-        if input_from.is_some(){
-            exec = exec.stdin(input_from.unwrap());
-        }
-        match exec.spawn()?.stdout{
-            Some(out) => return Ok(out.into()),
-            None => return Err(io::Error::new(io::ErrorKind::BrokenPipe, "Error: spawned process's stdout couldn't be grabbed")),
-        }
-        
+        let mut exec = Command::new(&self.invoking);
+        exec.args(&self.args);
 
+        if let Some(stdin) = input_from {
+            exec.stdin(stdin.into());
+        }
+
+        if let Some(stdout) = output_to {
+            exec.stdout(stdout.into());
+        }
+
+        exec.spawn()
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    use std::path::PathBuf;
+
+    use crate::tokens::{CommandParams, Terminator};
+
+    
+    #[test]
+    fn test_commands(){
+        let SampleLs: CommandParams = CommandParams{
+            invoking: PathBuf::from("/usr/bin/ls"),
+            args: vec!["-l".to_string()]
+        };
+
+        let SampleGrep: CommandParams = CommandParams { 
+            invoking: PathBuf::from("/usr/bin/grep"), 
+            args: vec!["rs".to_string()]
+        };
+
+        let command_list = vec![(SampleLs, Terminator::None), (SampleGrep, Terminator::Pipe)];
+        CommandParams::execute_statement(command_list).unwrap();
     }
 }
